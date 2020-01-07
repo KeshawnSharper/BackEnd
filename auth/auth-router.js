2072e70638ebac576b8e6f98a7602d1d3c972a05
Lambda_School_Loaner_182 <lambda_school_loaner_182@Lambda-School-Loaner-182s-MacBook-Pro.local>
2020-01-07 00:26:19 -0500

    still trying to deploy

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -40,7 +40,7 @@ router.post('/login', (req, res) => {
    }
   })
   .catch(err => {
-    res.status(500).json({ message: err });
+    res.status(500).json({ message: err })
     console.log(err)
   });
 });
